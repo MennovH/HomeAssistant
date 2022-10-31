d22cafcc34680278b8317f6bbb27e593912c503a
MennovH <36268286+MennovH@users.noreply.github.com>
2022-10-31 22:07:15 +0100

    Update run.sh

--- ddns/rootfs/run.sh
+++ ddns/rootfs/run.sh
@@ -69,7 +69,7 @@ do
             then
                 echo -e " - ${DOMAIN} (\e[1;31m${DOMAIN_IP}\e[1;37m),\e[1;31m failed to update\e[1;37m\n"
             else
-                echo -e " - ${DOMAIN} (\e[1;32m${DOMAIN_IP}\e[1;37m),\e[1;32m updated\e[1;37m\n"
+                echo -e " - ${DOMAIN} (\e[1;31m${DOMAIN_IP}\e[1;37m),\e[1;32m updated\e[1;37m\n"
             fi
         else
             echo -e " - ${DOMAIN}, up-to-date\n"
