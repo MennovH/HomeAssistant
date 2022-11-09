26bdf84e8d81bc4cffc50cd4c8ac2f72e8b01712
MennovH <36268286+MennovH@users.noreply.github.com>
2022-11-09 15:34:10 +0100

    Update run.sh

--- FallbackHTTP/rootfs/run.sh
+++ FallbackHTTP/rootfs/run.sh
@@ -45,7 +45,8 @@ do
         #echo -e $(openssl s_client -servername "${INTERNAL_IP}" -connect "${INTERNAL_IP}":"${INTERNAL_PORT}" 2>/dev/null | openssl x509 -noout -dates | grep -i notafter | cut -c 10-)
         TEST=$(echo | openssl s_client -servername "${INTERNAL_IP}" -connect "${INTERNAL_IP}:${INTERNAL_PORT}" 2>/dev/null | openssl x509 -noout -dates | grep -i notafter | cut -c 10-)
 
-        echo -e "${TEST}"...
+        echo -e "${TEST}"
+        echo -e "${date}"
 
         if [[ $(date -d "${date}") < $(date -d "${TEST}") ]];
         then
