9acfa942e405477f6135b818371c3f8333d2c0cc
MennovH <36268286+MennovH@users.noreply.github.com>
2022-11-09 15:32:33 +0100

    Update run.sh

--- FallbackHTTP/rootfs/run.sh
+++ FallbackHTTP/rootfs/run.sh
@@ -43,7 +43,7 @@ do
         
         echo "Testing certificate..."
         #echo -e $(openssl s_client -servername "${INTERNAL_IP}" -connect "${INTERNAL_IP}":"${INTERNAL_PORT}" 2>/dev/null | openssl x509 -noout -dates | grep -i notafter | cut -c 10-)
-        TEST=$(echo | openssl s_client -servername "${INTERNAL_IP}" -connect "${INTERNAL_IP}:${INTERNAL_PORT}" 2>/dev/null | openssl x509 -noout -dates | grep -i notafter | cut -c 10- | sed 's/  / /g')
+        TEST=$(echo | openssl s_client -servername "${INTERNAL_IP}" -connect "${INTERNAL_IP}:${INTERNAL_PORT}" 2>/dev/null | openssl x509 -noout -dates | grep -i notafter | cut -c 10-)
 
         echo -e "${TEST}"...
 
