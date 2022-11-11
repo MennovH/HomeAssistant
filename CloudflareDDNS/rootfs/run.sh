553c5d2f0a9b89a9c0784bbfefbed282b5acc6a6
MennovH <36268286+MennovH@users.noreply.github.com>
2022-11-11 05:27:09 +0100

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -46,8 +46,8 @@ do
     # iterate through listed domains
     for ITEM in $(bashio::config "domains|keys");
     do
-        DOMAIN=$(bashio::config "domains[${item}].domain")    
-        DNS_RECORD=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?type=A&name=${ITEM}&page=1&per_page=100&match=all" \
+        DOMAIN=$(bashio::config "domains[${ITEM}].domain")    
+        DNS_RECORD=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?type=A&name=${DOMAIN}&page=1&per_page=100&match=all" \
          -H "X-Auth-Email: ${EMAIL}" \
          -H "Authorization: Bearer ${TOKEN}" \
          -H "Content-Type: application/json")
