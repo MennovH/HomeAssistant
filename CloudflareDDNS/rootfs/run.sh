3f12f2bc4c6e3349ed30088bd1a0c7bf477e22cc
MennovH <36268286+MennovH@users.noreply.github.com>
2022-11-17 21:02:29 +0100

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -84,7 +84,7 @@ do
 
     done
     
-    NEXT=$(echo | date -d "${INTERVAL} minutes" '+%Y-%m-%d %H:%M')
+    NEXT=$(date -d "$(date '+%Y-%m-%d %H:%M') ${INTERVAL} minutes" '+%Y-%m-%d %H:%M')
     echo -e " \nNext check will run at ${NEXT}\n"
 
     if [[ ${INTERVAL} == 1 ]];
