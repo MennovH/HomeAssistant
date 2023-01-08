43254c435de312d3568dd12434092f8eaa6daa6d
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-08 23:12:04 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bashio
 
-#declare HOUR
-#declare QUARTER
 declare DAY
 declare RET
 
-#HOUR=$(bashio::config 'hour' | xargs echo -n)
-#QUARTER=$(bashio::config 'quarter' | xargs echo -n)
 DAY=$(bashio::config 'day' | xargs echo -n)
 
-echo "Running script\n"
+echo -e "Running script\n"
 RESULT=$(python3 run.py ${DAY})
 
 echo -e "${RESULT}\n"
-
 if [[ ${RESULT} == *"Restart"* ]];
 then
     bashio::core.restart
