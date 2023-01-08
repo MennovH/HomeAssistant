49bb9c2bf70889761492c421153a064bd9995a6c
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-08 02:22:55 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -10,7 +10,7 @@ DAY=$(bashio::config 'day' | xargs echo -n)
 
 while :
 do
-	at ${HOUR}:${QUARTER} -f /run.py ${DAY}
+	at -f /run.py ${DAY} ${HOUR}:${QUARTER}
 	NEXT=$(echo | busybox date -d@"$(( `busybox date +%s`+24*3600 ))" "+%Y-%m-%d %H:%M:%S")
     	echo -e " \nNext check is at ${NEXT}\n "
     	sleep ${INTERVAL}m
