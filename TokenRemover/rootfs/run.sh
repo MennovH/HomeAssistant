ed1b5973effb77311d6b2310547c0f7fe5bab1f3
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-08 17:43:59 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -11,9 +11,9 @@ DAY=$(bashio::config 'day' | xargs echo -n)
 echo "Running script"
 python3 /run.py ${DAY}
 echo "Done"
-#curl -X POST http://supervisor/core/restart -H "Authorization: Bearer $SUPERVISOR_TOKEN"
+curl -X POST http://supervisor/core/restart -H "Authorization: Bearer '$SUPERVISOR_TOKEN'"
 
-bashio::core.restart supervisor_token "${SUPERVISOR_TOKEN}"
+bashio::core.restart
 
 #while :
 #do
