470c9f80adea521c1b9e8f72a5a5fc75fbde7f88
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-08 21:35:17 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -9,21 +9,13 @@ declare RET
 #QUARTER=$(bashio::config 'quarter' | xargs echo -n)
 DAY=$(bashio::config 'day' | xargs echo -n)
 
-echo "Running script"
-output=$(python3 run.py ${DAY})
-if [[ ${output} == "restart" ]];
+echo "Running script\n"
+RESULT=$(python3 run.py ${DAY})
+
+echo -e "${RESULT}\n"
+
+if [[ ${RESULT} == *"Restart"* ]];
 then
     bashio::core.restart
 fi
 echo "Done"
-
-#
-#while :
-#do
-
-	#echo -e "/run.py ${DAY}" | at ${HOUR}:${QUARTER}
-	#at -f /run.py ${DAY} ${HOUR}:${QUARTER}
-	#NEXT=$(echo | busybox date -d@"$(( `busybox date +%s`+86400 ))" "+%Y-%m-%d %H:%M:%S")
-    	#echo -e " \nNext check is at ${NEXT}\n "
-    	#sleep 86400m
-#done
