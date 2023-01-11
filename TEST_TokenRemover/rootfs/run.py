32a21effa4699c9ee5754e73eaec578b8fbd32ac
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-11 01:49:56 +0100

    Update run.py

--- TEST_TokenRemover/rootfs/run.py
+++ TEST_TokenRemover/rootfs/run.py
@@ -58,7 +58,7 @@ if removed_tokens > 0:
         result = sp.check_output(f'cp {TMP_AUTH_FILE} {AUTH_FILE}', shell=1)
     # "send" return value to bash, so it will run the "ha core restart" command hereafter. The restart is
     # necessary to implement the changes, otherwise the updated file will be restored by Home Assistant RAM.
-    print(f"Home Assistant Core will now restart to remove {removed_tokens} token{'' if removed_tokens == 1 else 's'}")
+    print(f"Remove {removed_tokens} token{'' if removed_tokens == 1 else 's'}")
 else:
     print(f"No tokens older than {sys.argv[1]} day{'' if sys.argv[1] == 1 else 's'} were found")
     
