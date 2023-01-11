a962a304ed1c29068226cd738ac65c68496e3c09
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-11 01:11:45 +0100

    Update run.py

--- TEST - TokenRemover/rootfs/run.py
+++ TEST - TokenRemover/rootfs/run.py
@@ -44,7 +44,7 @@ if removed_tokens > 0:
     with open(TMP_AUTH_FILE, "w") as f:
         json.dump(data, f, indent=4)
     
-    sp.run(f'mv {TMP_AUTH_FILE} {AUTH_FILE}')
+    sp.run(f'mv {TMP_AUTH_FILE} {AUTH_FILE}', shell=1)
     # "send" return value to bash, so it will run the "ha core restart" command hereafter. The restart is
     # necessary to implement the changes, otherwise the updated file will be restored by Home Assistant RAM.
     # print(f"Home Assistant Core will now restart to remove {removed_tokens} token{'' if removed_tokens == 1 else 's'}")
