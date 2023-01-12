fcf81fe9d318ed46523351232b6733235ab5597f
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-12 02:09:16 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -4,7 +4,7 @@ declare DAY
 declare RESULT
 declare BAN
 
-BAN = "/config/ip_bans.yamls"
+BAN = "/config/ip_bans.yaml"
 DAY=$(bashio::config 'day' | xargs echo -n)
 
 echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S') > Running TokenRemover\n"
@@ -19,13 +19,26 @@ then
         cp /config/ip_bans.yaml /config/tmp_ip_bans.yaml   
     fi
     
+    BANNUM=$(wc -l "${BAN}")
+    
     sleep 0.75
     curl -X DELETE "http://supervisor/auth/cache" -H "Authorization: Bearer $SUPERVISOR_TOKEN"
     bashio::core.restart
     
-    if [ -f ${BAN} ];
+    runtime="5 minute"
+    endtime=$(date -ud "$runtime" +%s)
+
+    while [[ $(date -u +%s) -le $endtime ]]
+    do
+        echo "Aftermath: `date +%H:%M:%S`"
+        BANNUM2=$(wc -l "${BAN}")
+        sleep 30
+    done
+    
+    if [ -f ${BAN} && ${BANNUM2} > ${BANNUM}];
     then
-        cp /config/tmp_ip_bans.yaml /config/ip_bans.yaml && rm 
+        cp /config/tmp_ip_bans.yaml /config/ip_bans.yaml && rm
+        bashio::core.restart
     fi
 fi
 
