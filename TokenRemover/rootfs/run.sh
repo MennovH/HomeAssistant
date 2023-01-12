ef2a967cfb17d300f2b4b319a000428b1e54703f
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-12 03:14:45 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -14,7 +14,6 @@ RESULT=$(python3 run.py ${DAY})
 echo -e "${RESULT}\n"
 if [[ ${RESULT} == *"restart"* ]];
 then
-
     if [ -f "${BAN}" ];
     then
         cp "${BAN}" "${TMP_BAN}"
@@ -22,15 +21,8 @@ then
     fi
     
     sleep 0.75
-    # delete auth cache
     curl -X DELETE "http://supervisor/auth/cache" -H "Authorization: Bearer $SUPERVISOR_TOKEN" >/dev/null 2>&1
-    
-    # invoke restart of Home Assistant Core
     bashio::core.restart
-
-    # run the following procedure to re-enable locked out users after running TokenRemover
-    # this could happen when [multiple] devices try to re-authenticate to Home Assistant with a revoked token
-    # e.g. when "Keep me logged in" was set
     
     echo "Aftermath: `date +%H:%M:%S`"
     for i in {1..3};
@@ -46,14 +38,11 @@ then
         then
             echo -e "${BANNUM}"
             echo -e "${BANNUM2}"
-            # restore ip_bans.yaml file, restart ha core again to make changes persistent
-            
             echo "Detected banned IP addresses since execution.\nRestoring ip_bans.yaml file."
             cp "${TMP_BAN}" ${BAN} && rm "${TMP_BAN}"
             
             bashio::core.restart
         else
-            # remove temporary ip_bans.yaml file
             rm "${TMP_BAN}";
     fi
 fi
