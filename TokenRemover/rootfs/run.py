7bc9b1cf84f209ecc87a8bf0039168544417708a
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-13 00:33:24 +0100

    Update run.py

--- TokenRemover/rootfs/run.py
+++ TokenRemover/rootfs/run.py
@@ -25,12 +25,12 @@ for token in data["data"]["refresh_tokens"]:
         keep_list.append(token)
         continue
     
-    if ACTIVE_DAYS < 999:
+    if DAYS_ACTIVE < 999:
         date_str = token["last_used_at"]
         year, month, day, hour, minute, second = date_str[:date_str.index(".")].translate(date_str.maketrans("T:.", "---")).split("-")
         last_used_date = dt.datetime(int(year), int(month), int(day), int(hour), int(minute))
     
-        if last_used_date >= (dt.datetime.now() + dt.timedelta(minutes=30) - dt.timedelta(days=int(ACTIVE_DAYS))):
+        if last_used_date >= (dt.datetime.now() + dt.timedelta(minutes=30) - dt.timedelta(days=int(DAYS_ACTIVE))):
             keep_list.append(token)
             continue
 
