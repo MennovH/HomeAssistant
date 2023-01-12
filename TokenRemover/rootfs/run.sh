f1196e9a2a114661ca8a77cdc7a1fd32cca6b424
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-12 01:57:29 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -2,7 +2,9 @@
 
 declare DAY
 declare RESULT
+declare BAN
 
+BAN = "/config/ip_bans.yamls"
 DAY=$(bashio::config 'day' | xargs echo -n)
 
 echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S') > Running TokenRemover\n"
@@ -11,9 +13,20 @@ RESULT=$(python3 run.py ${DAY})
 echo -e "${RESULT}\n"
 if [[ ${RESULT} == *"restart"* ]];
 then
+
+    if [ -f ${BAN} ];
+    then
+        cp /config/ip_bans.yaml /config/tmp_ip_bans.yaml   
+    fi
+    
     sleep 0.75
     curl -X DELETE "http://supervisor/auth/cache" -H "Authorization: Bearer $SUPERVISOR_TOKEN"
     bashio::core.restart
+    
+    if [ -f ${BAN} ];
+    then
+        cp /config/tmp_ip_bans.yaml /config/ip_bans.yaml && rm 
+    fi
 fi
 
 echo -e "Finished TokenRemover execution"
