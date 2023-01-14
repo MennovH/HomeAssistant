fe0da60885899a0c4498cf10fecd3504b953402d
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-14 01:54:24 +0100

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -1,50 +1,20 @@
 #!/usr/bin/env bashio
 
-declare DAY
-declare RESULT
-declare BAN
-declare LAST_USED
-declare KEEP_ACTIVE
-declare ACTIVE_DAYS
+declare BAN_FILE
+declare TMP_BAN_FILE
+declare BAN_LINE_COUNT
+declare TMP_BAN_LINE_COUNT
+
 declare ACTIVATION_DAYS
+declare KEEP_ACTIVE
 declare RETENTION_DAYS
+declare RESULT
 
-
-BAN="/config/ip_bans.yaml"
-TMP_BAN="/config/tmp_ip_bans.yaml"
-#DAY=$(bashio::config 'day' | xargs echo -n)
-
-
-
-if [[ -v $(bashio::config)["day"] ]];
-then
-    RETENTION_DAYS=$(bashio::config 'day' | xargs echo -n)
-    # codeDict has ${STR_ARRAY[2]} as a key
-else
-    # codeDict does not have ${STR_ARRAY[2]} as a key
-    RETENTION_DAYS=$(bashio::config 'retention_days' | xargs echo -n)
-fi
-
-if [[ -v $(bashio::config)["last_used"] ]];
-then
-    KEEP_ACTIVE=$(bashio::config 'last_used' | xargs echo -n)
-    # codeDict has ${STR_ARRAY[2]} as a key
-else
-    # codeDict does not have ${STR_ARRAY[2]} as a key
-    KEEP_ACTIVE=$(bashio::config 'keep_active' | xargs echo -n)
-fi
-
-if [[ -v $(bashio::config)["last_used"] ]];
-then
-    ACTIVATION_DAYS=$(bashio::config 'last_used' | xargs echo -n)
-    # codeDict has ${STR_ARRAY[2]} as a key
-else
-    # codeDict does not have ${STR_ARRAY[2]} as a key
-    ACTIVATION_DAYS=$(bashio::config 'activation_days' | xargs echo -n)
-fi
-
-#LAST_USED=$(bashio::config 'last_used' | xargs echo -n)
-#ACTIVE_DAYS=$(bashio::config 'active_days' | xargs echo -n)
+BAN_FILE="/config/ip_bans.yaml"
+TMP_BAN_FILE="/config/tmp_ip_bans.yaml"
+RETENTION_DAYS=$(bashio::config 'retention_days' | xargs echo -n)
+KEEP_ACTIVE=$(bashio::config 'keep_active' | xargs echo -n)
+ACTIVATION_DAYS=$(bashio::config 'activation_days' | xargs echo -n)
 
 echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S')\n"
 echo -e "Running TokenRemover\n"
@@ -60,10 +30,10 @@ fi
 echo -e " \n${RESULT}\n"
 if [[ ${RESULT} == *"restart"* ]];
 then
-    if [ -f "${BAN}" ];
+    if [ -f "${BAN_FILE}" ];
     then
-        cp "${BAN}" "${TMP_BAN}"
-        BANNUM=$(wc -l "${BAN}")
+        cp "${BAN_FILE}" "${TMP_BAN_FILE}"
+        BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
     fi
     
     sleep 0.75
@@ -76,19 +46,19 @@ then
         sleep 15
     done
 
-    if [ -f "${BAN}" ];
+    if [ -f "${BAN_FILE}" ];
     then
-        BANNUM2=$(wc -l "${BAN}")
-        if ! [[ ${BANNUM} == ${BANNUM2} ]];
+        TMP_BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
+        if ! [[ ${BAN_LINE_COUNT} == ${TMP_BAN_LINE_COUNT} ]];
         then
             echo -e "\e[1;31mDetected banned IP addresses since execution.\nRestoring ip_bans.yaml file.\e[1;37m\n"
-            cp "${TMP_BAN}" "${BAN}" && rm "${TMP_BAN}"
+            cp "${TMP_BAN_FILE}" "${BAN_FILE}" && rm "${TMP_BAN_FILE}"
             
             bashio::core.restart
             
             echo -e " \nFinished restoring ip_bans.yaml file.\n"
         else
-            rm "${TMP_BAN}";
+            rm "${TMP_BAN_FILE}";
         fi
     fi
 fi
