2932587d806c98b4e2d457a8026b0589b56514b5
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-23 12:38:41 +0100

    Update run.py

--- TEST_TokenRemover/rootfs/run.py
+++ TEST_TokenRemover/rootfs/run.py
@@ -10,8 +10,8 @@ AUTH_FILE = "/config/.storage/auth"
 
 
 def addon(info):
-    print(info)
-    sys.exit(0)
+    return info
+    
     prt = info[info.index("TEST_TokenRemover"):]
     return prt[prt.index(": "):prt.index("\", \"description")][3:]
 
