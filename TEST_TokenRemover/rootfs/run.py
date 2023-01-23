8094cff2dfcf1549b8579bcb21b8b2b07f365fe9
MennovH <mc_vanheerde@hotmail.com>
2023-01-23 03:38:03 +0100

    Update run.py

--- TEST_TokenRemover/rootfs/run.py
+++ TEST_TokenRemover/rootfs/run.py
@@ -90,7 +90,7 @@ def tokenremover(retention_days, active_days):
         # "send" return value to bash, so it will run the "ha core restart" command hereafter. The restart is
         # necessary to implement the changes, otherwise the updated file will be restored by Home Assistant RAM.
     
-    return f" -> Removed {removed_tokens} token{'' if removed_tokens == 1 else 's'}{'\n -> Restarting...' if removed_tokens >0 else ''}"
+    return f" -> Removed {removed_tokens} token{'' if removed_tokens == 1 else 's'}" + "\n" + f"{ -> Restarting...' if removed_tokens >0 else ''}"
     
 
 if __name__ == '__main__':    
