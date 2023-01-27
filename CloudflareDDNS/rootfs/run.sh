cc7a27528db6ff18934989c81b5bdda6d441417c
MennovH <36268286+MennovH@users.noreply.github.com>
2023-01-27 01:28:13 +0100

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -19,7 +19,7 @@ CROSS_MARK="\u274c"
 
 for ITEM in $(bashio::config "domains|keys");
 do
-    ARR+= $(echo -e $(bashio::config "domains[${ITEM}].domain"))
+    ARR+= $(bashio::config "domains[${ITEM}].domain")
 done | sort -uk 1
 
 echo -e ${ARR[@]}
