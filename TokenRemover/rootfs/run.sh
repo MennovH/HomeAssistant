d6661bc2607ce7b20ce46ac51ab758a8fd09f8cd
MennovH <36268286+MennovH@users.noreply.github.com>
2023-03-30 20:10:32 +0200

    Update run.sh

--- TokenRemover/rootfs/run.sh
+++ TokenRemover/rootfs/run.sh
@@ -9,59 +9,113 @@ declare ACTIVATION_DAYS
 declare KEEP_ACTIVE
 declare RETENTION_DAYS
 declare RESULT
+declare AUTO
+declare ADDON
+declare MON
+declare TUE
+declare WED
+declare THU
+declare FRI
+declare SAT
+declare SUN
 
 BAN_FILE="/config/ip_bans.yaml"
 TMP_BAN_FILE="/config/tmp_ip_bans.yaml"
 RETENTION_DAYS=$(bashio::config 'retention_days' | xargs echo -n)
 KEEP_ACTIVE=$(bashio::config 'keep_active' | xargs echo -n)
 ACTIVATION_DAYS=$(bashio::config 'activation_days' | xargs echo -n)
-
-echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S')\n"
-echo -e "Running TokenRemover\n"
+AM_PM=$(bashio::config 'am_pm' | xargs echo -n)
+AUTOMATION_TIME=$(bashio::config 'automation_time' | xargs echo -n)
+MON=$(bashio::config 'mon' | xargs echo -n)
+TUE=$(bashio::config 'tue' | xargs echo -n)
+WED=$(bashio::config 'wed' | xargs echo -n)
+THU=$(bashio::config 'thu' | xargs echo -n)
+FRI=$(bashio::config 'fri' | xargs echo -n)
+SAT=$(bashio::config 'sat' | xargs echo -n)
+SUN=$(bashio::config 'sun' | xargs echo -n)
 
 if [ "${KEEP_ACTIVE}" == false ];
 then
-    echo -e " \nNote: You may get locked out for one minute after restart, as TokenRemover doesn't know which token belongs to whom. TokenRemover will restore the current ip_bans.yaml file when it detects newly banned IP addresses within one minute after execution. Home Assistant Core will then again be restarted to make this change permanent, after which you should be able to log in again.\n"
-    RESULT=$(python3 run.py ${RETENTION_DAYS} 999)
-else
-    RESULT=$(python3 run.py ${RETENTION_DAYS} ${ACTIVATION_DAYS})
+    echo -e "${__BASHIO_COLORS_YELLOW}Note: You may get locked out for one minute after restart, as TokenRemover doesn't know which token belongs to whom. TokenRemover will restore the current ip_bans.yaml file when it detects newly banned IP addresses within one minute after execution. Home Assistant Core will then again be restarted to make this change permanent, after which you should be able to log in again.\n${__BASHIO_COLORS_DEFAULT}"
+	ACTIVATION_DAYS = 999
 fi
 
-echo -e " \n${RESULT}\n"
-if [[ ${RESULT} == *"restart"* ]];
+AUTO="Once"
+for day in "${MON}" "${TUE}" "${WED}" "${THU}" "${FRI}" "${SAT}" "${SUN}";
+do
+	if [ "${day}" == true ];
+	then
+		AUTO="Defined"
+		break
+	fi
+done
+
+run () {
+
+    echo -e " \nRun time: $(date '+%Y-%m-%d %H:%M:%S')\n"	
+	
+	RESULT=$(python3 run.py 1 ${RETENTION_DAYS} ${ACTIVATION_DAYS})
+	
+	if [[ ${RESULT} == *"Restart"* ]];
+	then
+		if [ -f "${BAN_FILE}" ];
+		then
+			cp "${BAN_FILE}" "${TMP_BAN_FILE}"
+			BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
+		fi
+		echo -e "${RESULT}"
+		
+		sleep 0.75
+		
+		# restart Home Assistant Core
+		bashio::core.restart
+		
+		echo -e -n " -> Running checks..."
+		sleep 120
+		echo -e "\r Done\n"
+			
+		if [ -f "${BAN_FILE}" ];
+		then
+			TMP_BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
+			if ! [[ ${BAN_LINE_COUNT} == ${TMP_BAN_LINE_COUNT} ]];
+			then
+				echo -e "${__BASHIO_COLORS_YELLOW} -> Detected new IP bans.${__BASHIO_COLORS_DEFAULT}\n    Restoring ip_bans.yaml file.\n"
+				cp "${TMP_BAN_FILE}" "${BAN_FILE}" && rm "${TMP_BAN_FILE}"
+				
+				bashio::core.restart
+				
+				echo -e "${__BASHIO_COLORS_GREEN} -> Restored ip_bans.yaml file.${__BASHIO_COLORS_DEFAULT}\n"
+			else
+				rm "${TMP_BAN_FILE}";
+			fi
+		fi
+	else
+		echo -e "${RESULT}"
+	fi
+	echo -e " -> Finished TokenRemover execution\n "
+}
+
+if [ "${AUTO}" == "Once" ];
 then
-    if [ -f "${BAN_FILE}" ];
-    then
-        cp "${BAN_FILE}" "${TMP_BAN_FILE}"
-        BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
-    fi
-    
-    sleep 0.75
-    #curl -X DELETE "http://supervisor/auth/cache" -H "Authorization: Bearer $SUPERVISOR_TOKEN" >/dev/null 2>&1
-    curl -X DELETE -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/auth/cache >/dev/null 2>&1
-    bashio::core.restart
-    
-    echo -e "(still) Running checks...\n"
-    for i in {1..4};
-    do
-        sleep 15
-    done
-
-    if [ -f "${BAN_FILE}" ];
-    then
-        TMP_BAN_LINE_COUNT=$(wc -l "${BAN_FILE}")
-        if ! [[ ${BAN_LINE_COUNT} == ${TMP_BAN_LINE_COUNT} ]];
-        then
-            echo -e "\e[1;31mDetected banned IP addresses since execution.\nRestoring ip_bans.yaml file.\e[1;37m\n"
-            cp "${TMP_BAN_FILE}" "${BAN_FILE}" && rm "${TMP_BAN_FILE}"
-            
-            bashio::core.restart
-            
-            echo -e " \nFinished restoring ip_bans.yaml file.\n"
-        else
-            rm "${TMP_BAN_FILE}";
-        fi
-    fi
+	echo -e "${__BASHIO_COLORS_YELLOW}TokenRemover will run only once due to invalid recurrence configuration${__BASHIO_COLORS_DEFAULT}"
+	
+	# Get current addon name
+	ADDON=$(curl -X GET --silent -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/addons)
+	ADDON=$(python3 run.py 2 ${ADDON})
+	run
+	
+	# Permanently stop this addon from running
+	$(curl -X POST --silent -H "Authorization: Bearer $SUPERVISOR_TOKEN" http://supervisor/addons/${ADDON}/stop > /dev/null 2>&1)
+	
+else
+	echo -e "${__BASHIO_COLORS_GREEN}TokenRemover will run at set times${__BASHIO_COLORS_DEFAULT}\n "
+	while :
+	do
+		RESULT=$(python3 run.py 0 ${AM_PM} ${AUTOMATION_TIME} ${MON} ${TUE} ${WED} ${THU} ${FRI} ${SAT} ${SUN})
+		echo -e $(echo -e "${RESULT}" | head -n1)
+		sleep $(echo -e "${RESULT}" | tail -n1)
+		
+		run
+		sleep 60	
+	done
 fi
-
-echo "Finished TokenRemover execution"
