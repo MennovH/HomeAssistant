093da2f4f598fde05000714a7d201f21383e027a
MennovH <36268286+MennovH@users.noreply.github.com>
2023-06-15 02:49:34 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -171,7 +171,7 @@ if [[ ${INTERVAL} == 1 ]]; then bashio::log.info "Iterating every minute\n "; el
 
 while :
 do
-    PUBLIC_IP=$(wget -O - -q -t 1 https://api.ipify2.org 2>/dev/null)
+    PUBLIC_IP=$(wget -O - -q -t 1 https://api.ipify.org 2>/dev/null)
     echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S')"
 
     if [[ ! -z "$PUBLIC_IP" ]];
