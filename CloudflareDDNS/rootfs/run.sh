04efeb20a271b9bd2913c8f2d0c176a42fec207d
MennovH <36268286+MennovH@users.noreply.github.com>
2023-06-16 00:18:34 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -174,55 +174,69 @@ while :
 do
 
     echo -e "Runtime errors: ${PIP_ERRORS}/${ITERATION_ERRORS}/${CREATION_ERRORS}/${UPDATE_ERRORS}"
+
+    success=0
+    while :
+    do
+        for i in "api.ipify.org" "api.my-ip.io/ip"
+        do PUBLIC_IP=$(curl -s --connect-timeout 5 https://$i || echo 0)
+            if [[ $PUBLIC_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+            then
+                success=1
+                break
+            fi
+        done
+        if [[ $success == 1 ]]
+        then
+            break
+        fi
+        sleep 10s
+    done
+
+    echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S')"
+
     ISSUE=0
     #PUBLIC_IP=$(wget -O - -q -t 1 https://api.ipify.org 2>/dev/null)
-    PUBLIC_IP=$(curl -s --connect-timeout 50 https://api.ipify.org || echo 0)
+    # PUBLIC_IP=$(curl -s --connect-timeout 50 https://api.ipify.org || echo 0)
     
-    echo -e "Time: $(date '+%Y-%m-%d %H:%M:%S')"
-    if [[ $PUBLIC_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
+    # if [[ $PUBLIC_IP =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
     #if [[ $PUBLIC_IP != 0  ]];
+    # then
+    NEXT=$(echo | busybox date -d@"$(( `busybox date +%s`+${INTERVAL}*60 ))" "+%Y-%m-%d %H:%M:%S")
+    SECONDS=0
+    echo -e "Next: ${NEXT}"
+    if [[ ${HIDE_PIP} == false ]]; then echo -e "Public IP address: ${BL}${PUBLIC_IP}${N}\n"; fi
+    
+    # fetch existing A records
+    DOMAINS=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?type=A" \
+        -H "Authorization: Bearer ${TOKEN}" \
+        -H "Content-Type: application/json" | jq -r '.result[].name')
+    
+    if [[ ! -z "$DOMAINS" ]];
     then
-        NEXT=$(echo | busybox date -d@"$(( `busybox date +%s`+${INTERVAL}*60 ))" "+%Y-%m-%d %H:%M:%S")
-        SECONDS=0
-        echo -e "Next: ${NEXT}"
-        if [[ ${HIDE_PIP} == false ]]; then echo -e "Public IP address: ${BL}${PUBLIC_IP}${N}\n"; fi
-        
-        # fetch existing A records
-        DOMAINS=$(curl -sX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?type=A" \
-            -H "Authorization: Bearer ${TOKEN}" \
-            -H "Content-Type: application/json" | jq -r '.result[].name')
-        
-        if [[ ! -z "$DOMAINS" ]];
+        count=$(wc -w <<< $PERSISTENT_DOMAINS)
+        if [[ $count > 0 ]];
         then
-            count=$(wc -w <<< $PERSISTENT_DOMAINS)
-            if [[ $count > 0 ]];
-            then
-                # add persistent domains to obtained record list
-                for DOMAIN in ${PERSISTENT_DOMAINS[@]};
-                do 
-                    TMP_DOMAIN=$(sed "s/_no_proxy/""/" <<< "$DOMAIN")
-                    if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"`;
-                    then
-                        DOMAINS+=("$DOMAIN")
-                    fi
-                    PERSISTENT_DOMAINS=( "${PERSISTENT_DOMAINS[@]/$DOMAIN/}" )
-                done
-            fi
+            # add persistent domains to obtained record list
+            for DOMAIN in ${PERSISTENT_DOMAINS[@]};
+            do 
+                TMP_DOMAIN=$(sed "s/_no_proxy/""/" <<< "$DOMAIN")
+                if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"`;
+                then
+                    DOMAINS+=("$DOMAIN")
+                fi
+                PERSISTENT_DOMAINS=( "${PERSISTENT_DOMAINS[@]/$DOMAIN/}" )
+            done
+        fi
+    
+        DOMAIN_LIST=($(for D in "${DOMAINS[@]}"; do echo "${D}"; done | sort -u))
         
-            DOMAIN_LIST=($(for D in "${DOMAINS[@]}"; do echo "${D}"; done | sort -u))
-            
-            if [[ ! -z "$DOMAIN_LIST" ]];
-            then
-                # iterate through listed domains
-                ITERATION=$(($ITERATION + 1))
-                echo "Domain list iteration ${ITERATION}:"
-                for DOMAIN in ${DOMAIN_LIST[@]}; do check ${DOMAIN}; done
-            else
-                # iteration failed
-                ITERATION_ERRORS=$(($ITERATION_ERRORS + 1))
-                echo -e "${RR}Domain list iteration failed. Retrying in 60 seconds...${N}"
-                ISSUE=1
-            fi
+        if [[ ! -z "$DOMAIN_LIST" ]];
+        then
+            # iterate through listed domains
+            ITERATION=$(($ITERATION + 1))
+            echo "Domain list iteration ${ITERATION}:"
+            for DOMAIN in ${DOMAIN_LIST[@]}; do check ${DOMAIN}; done
         else
             # iteration failed
             ITERATION_ERRORS=$(($ITERATION_ERRORS + 1))
@@ -230,11 +244,17 @@ do
             ISSUE=1
         fi
     else
-        # PIP fetch failed
-        PIP_ERRORS=$(($PIP_ERRORS + 1))
-        echo -e "${RR}Failed to get current public IP address. Retrying in 60 seconds...${N}"
+        # iteration failed
+        ITERATION_ERRORS=$(($ITERATION_ERRORS + 1))
+        echo -e "${RR}Domain list iteration failed. Retrying in 60 seconds...${N}"
         ISSUE=1
     fi
+    # else
+    #     # PIP fetch failed
+    #     PIP_ERRORS=$(($PIP_ERRORS + 1))
+    #     echo -e "${RR}Failed to get current public IP address. Retrying in 60 seconds...${N}"
+    #     ISSUE=1
+    # fi
     if [[ $ISSUE == 1 ]]
     then
         ISSUE=0
