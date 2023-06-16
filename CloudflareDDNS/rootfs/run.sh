8055f8e2401b5d06e94e81ddd8b75e1d1e4df7e5
MennovH <36268286+MennovH@users.noreply.github.com>
2023-06-16 13:15:53 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -238,7 +238,7 @@ do
             do
                 TMP_DOMAIN=$(sed "s/_no_proxy/""/" <<< "$DOMAIN")
                 #$DOMAINS=( "${DOMAINS[@]/$TMP_DOMAIN}" )
-                $DOMAINS = $(echo ${DOMAINS[@]/$TMP_DOMAIN} 2>/dev/null)
+                DOMAINS=echo "${DOMAINS[@]/$TMP_DOMAIN}"
                 if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"` ;
                 then
                     DOMAINS+=("$DOMAIN")
