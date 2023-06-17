bd210a601a6b34f2efd7c864f624214c28dd8067
MennovH <36268286+MennovH@users.noreply.github.com>
2023-06-17 03:48:38 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -47,7 +47,7 @@ BB="\e[1;34m" #bold blue
 BG="\e[1;32m" #bold green
 R="\e[1;31m" #bold red (error)
 
-echo "${RY}☁${N} Initializing add-on ☁"
+echo -e "${RY}☁${N} Initializing add-on ☁"
 
 # checks on configuration
 if [[ ${#ZONE} == 0 ]];
