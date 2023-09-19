9e3f6a062c5bdcab05cc26f586700903bed88fd3
MennovH <36268286+MennovH@users.noreply.github.com>
2023-09-19 11:57:55 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -31,6 +31,8 @@ NEW_PIP_COUNTER=0
 UPDATE_COUNTER=0
 CREATION_COUNTER=0
 
+UDE=""
+
 # font
 N="\e[0m" #normal
 I="\e[3m" #italic
@@ -159,7 +161,7 @@ function cfapi {
         
         if [[ ${PUBLIC_IP} != ${DOMAIN_IP} ]] && [[ ! -z ${DOMAIN_IP} ]];
         then
-            # UDE = "Update: ${PUBLIC_IP} != ${DOMAIN_IP}"
+            UDE="Update: ${PUBLIC_IP} != ${DOMAIN_IP}"
             # domain needs to be updated
             DATA=$(printf '{"type":"A","name":"%s","content":"%s","proxied":%s}' "${DOMAIN}" "${PUBLIC_IP}" "${DOMAIN_PROXIED}")
             API_RESPONSE=$((curl -sX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${DOMAIN_ID}" \
@@ -305,7 +307,7 @@ do
         ISSUE=1
     fi
     
-    # echo -e "$UDE"
+    echo -e "$UDE"
     # set sleep time and wait until next iteration
     sleep $(if [[ $ISSUE == 1 ]]; then echo 60; elif [[ $(((($INTERVAL*60)-($SECONDS/60))-($SECONDS%60))) -le 1 ]]; then echo -e $INTERVAL; else echo -e $(((($INTERVAL*60)-($SECONDS/60))-($SECONDS%60))); fi)s
     echo -e "\n "
