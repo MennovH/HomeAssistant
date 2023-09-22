deb0724b4222447c53db20cd5780a0d28e02aae6
MennovH <36268286+MennovH@users.noreply.github.com>
2023-09-23 00:42:14 +0200

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -102,8 +102,8 @@ function cfapi {
         -H "Authorization: Bearer ${TOKEN}" \
         -H "Content-Type: application/json") || echo 0)
 
-    RESP=$(response "1" "$API_RESPONSE")
-    echo $?
+    response "1" "$API_RESPONSE"
+    echo -e "$?"
     if [[ ${API_RESPONSE} == 0 ]];
     then
         ITERATION_ERRORS=$(($ITERATION_ERRORS + 1))
