70fd4e4c279c2068bf14cf67297dd660d80493fa
MennovH <36268286+MennovH@users.noreply.github.com>
2023-12-02 01:51:07 +0100

    Update run.sh

--- CloudflareDDNS/rootfs/run.sh
+++ CloudflareDDNS/rootfs/run.sh
@@ -276,13 +276,18 @@ do
                 DOMAINS=( "${DOMAINS[@]/$DOMAIN/}" )
                 if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"`; then DOMAINS+=("$DOMAIN"); fi
             done
+
             
-            # remove excluded domains from obtained record list
-            for DOMAIN in ${TMP_EXCLUDED_DOMAINS[@]};
-            do
-                TMP_DOMAIN=$(sed "s/_no_proxy/""/" <<< "$DOMAIN")
-                if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"`; then DOMAINS-=("$DOMAIN"); fi
-            done
+            count2=$(wc -w <<< $PERSISTENT_DOMAINS)
+            if ([[ ! -z "$DOMAINS" ]] && [[ $DOMAINS != 0 ]]) || [[ $count > 0 ]];
+            then
+                    # remove excluded domains from obtained record list
+                    for DOMAIN in ${TMP_EXCLUDED_DOMAINS[@]};
+                    do
+                        TMP_DOMAIN=$(sed "s/_no_proxy/""/" <<< "$DOMAIN")
+                        if `domain_lookup "$DOMAINS" "$TMP_DOMAIN"`; then DOMAINS-=("$DOMAIN"); fi
+                    done
+            fi
         fi
 
         # sort domain list alphabetically
