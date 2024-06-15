8b619feaaf1d023549baad50fc965882a8b1f859
MennovH <36268286+MennovH@users.noreply.github.com>
2024-06-15 15:28:51 +0200

    Update run.py

--- TokenRemover-test/rootfs/run.py
+++ TokenRemover-test/rootfs/run.py
@@ -113,10 +113,11 @@ def tokenremover(retention_days, active_days):
 if __name__ == '__main__':    
     if sys.argv[1] == '0':
         # Check recurrence
-        weekdays = [day-1 for day in range(len(sys.argv[3:])) if sys.argv[3:][day] == 'true']
+        weekdays = [day-1 for day in range(len(sys.argv[6:])) if sys.argv[6:][day] == 'true']
         print(sys.argv)
         print(sys.argv[2])
         print(sys.argv[3])
+        #['run.py', '0', 'Night', '3:30', '/', '15:30', 'true', 'true', 'true', 'true', 'true', 'true', 'true']
         result = recurrence(sys.argv[2], sys.argv[3].split(':'), weekdays)
     elif sys.argv[1] == '1':
         # Run tokenremover       
